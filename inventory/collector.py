dddabcd7df421b918e56e5e6767728fed1f2df86
gbasil93 <gbasil93@gmail.com>
2026-02-18 01:37:37 +0000

    Added pip source

--- inventory/collector.py
+++ inventory/collector.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
-from .sources import LinuxSource, DpkgSource
+from .sources import LinuxSource, DpkgSource, PipSource
 from .inventory import Inventory, InventoryItem
 
 
 def main():
-    inventory = Inventory([LinuxSource(release_file="/etc/os-release"), DpkgSource()])
+    inventory = Inventory(
+        [LinuxSource(release_file="/etc/os-release"), DpkgSource(), PipSource()]
+    )
     inventory.collect()
     inventory.print()
 
--- inventory/sources/__init__.py
+++ inventory/sources/__init__.py
@@ -1,2 +1,3 @@
 from .linux import LinuxSource
 from .dpkg import DpkgSource
+from .pip import PipSource
